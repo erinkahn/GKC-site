8b2382f463431c2b8c2fe592ef53a3cf8c1cb305
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-11 18:33:29 -0400

    some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 console.log("Hello World from main.js!");
 
 
-/* -----burger accordian menu------*/
+/* -----hamburger accordian menu------*/
 $(function() {
   function slideMenu() {
+    
     var activeState = $("#menu-container .menu-list").hasClass("active");
     $("#menu-container .menu-list").animate(
       {
@@ -11,11 +12,11 @@ $(function() {
       },
       400
     );
-  }
+  } 
   $("#menu-wrapper").click(function(event) {
     event.stopPropagation();
     $("#hamburger-menu").toggleClass("open");
-    $("#menu-container .menu-list").toggleClass("active");
+    $("#menu-container .menu-list").toggleClass("active"); //toggle class of active
     slideMenu();
 
     $("body").toggleClass("overflow-hidden");
@@ -42,20 +43,20 @@ $(function() {
 
  // tabbed content
     // http://www.entheosweb.com/tutorials/css/tabs.asp
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
+    $(".tab_content").hide(); // hide the rest of the tabs
+    $(".tab_content:first").show(); // show this tab first
 
   /* if in tab mode */
-    $("ul.tabs li").click(function() {
+    $("ul.tabs li").click(function() { // click tab item/li
     
-      $(".tab_content").hide();
+      $(".tab_content").hide(); //hide content
       var activeTab = $(this).attr("rel"); 
       $("#"+activeTab).fadeIn();    
     
-      $("ul.tabs li").removeClass("active");
-      $(this).addClass("active");
+      $("ul.tabs li").removeClass("active"); //remove active class from tab/li
+      $(this).addClass("active"); //add class of active to tab/li
 
-    $(".tab_drawer_heading").removeClass("d_active");
+    $(".tab_drawer_heading").removeClass("d_active"); 
     $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
     
     });
@@ -67,11 +68,11 @@ $(function() {
       var d_activeTab = $(this).attr("rel"); 
       $("#"+d_activeTab).fadeIn();
     
-    $(".tab_drawer_heading").removeClass("d_active");
-      $(this).addClass("d_active");
+    $(".tab_drawer_heading").removeClass("d_active"); // tab heading remove active class
+      $(this).addClass("d_active"); //add class of active 
     
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+    $("ul.tabs li").removeClass("active"); //remove active class 
+    $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active"); //add active class
     });
   
   
